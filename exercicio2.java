a429f4f78816b6c2c6970067aec4f36258aee938
yurisoares2077BTC <yurisoares2077@gmail.com>
2026-09-03 09:55:28 -0300

    Add exercicio2.java with basic output statements

---
+++ exercicio2.java
@@ -0,0 +1,7 @@
+public class exercicio2 {
+    public static void main (String[] args){
+
+        System.out.println("Minha idade é " + 19 + " anos");
+        System.out.println("Eu tenho " + 1.77 + " de altura ");
+        System.out.println("Status na aula: Presente");
+    }
