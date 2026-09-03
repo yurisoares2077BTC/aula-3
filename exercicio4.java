3ee57b75a9fdef47416c4662f0833665f7a98b72
yurisoares2077BTC <yurisoares2077@gmail.com>
2026-09-03 10:02:42 -0300

    exercicio4.java

---
+++ exercicio4.java
@@ -0,0 +1,9 @@
+public class exercicio4 {
+    public static void main(String[] args) {
+
+        int numero1 = 10;
+        int numero2 = 20;
+
+        System.out.printf("%d%n", numero1);
+        System.out.printf("%d%n", numero2);
+    }
