e10256790162be14b1620dbf397140b5e18231a9
yurisoares2077BTC <yurisoares2077@gmail.com>
2026-09-03 09:53:40 -0300

    Add exercicio1 class with main method

---
+++ exercicio3.java
@@ -0,0 +1,7 @@
+public class exercicio1 {
+    public static void main (String[] args){
+       
+        System.out.println("Yuri Soares da Silva");
+        System.out.println("06324504");
+        System.out.println("Análise e Desenvolvimento de Sistemas");
+    }
