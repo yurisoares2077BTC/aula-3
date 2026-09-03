cb2319c7017d72a3d5bb2f67014b324b151e8353
yurisoares2077BTC <yurisoares2077@gmail.com>
2026-09-03 10:00:57 -0300

    exercicio da tabuada

---
+++ exerciciotabuada.java
@@ -0,0 +1,6 @@
+public class exercicio3 {
+    public static void main(String[] args) {
+        int numero = 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d%n", numero, i, numero * i);
+        }
